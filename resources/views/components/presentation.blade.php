66cd3b1b5e79a607acc8f3fac587f4f3b3814235
aimariela <contact@marielaai.com>
2025-04-20 21:38:21 -0500

    cambiando texto personalizado

--- resources/views/components/presentation.blade.php
+++ resources/views/components/presentation.blade.php
@@ -1,27 +1,30 @@
-<section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
+<section class="py-10 bg-gray-100 dark:bg-zinc-900 sm:py-16 lg:py-24">
     <div class="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
         <div class="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-24 gap-y-10">
             <div>
                 <img class="object-cover w-16 h-16 rounded-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/cta/9/female-avatar-1.png" alt="" />
                 <blockquote>
-                    <p class="mt-6 text-xl leading-relaxed text-black">“Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.”</p>
+                    <p class="mt-6 text-xl leading-relaxed text-black">“ Whether you're launching a new brand or optimizing an existing platform, we bring innovation and strategy together to turn your vision into reality.
+                        Let us help you build smarter, faster, and more impactful web experiences.”</p>
                 </blockquote>
-                <p class="mt-6 text-base font-semibold text-black">Jenny Wilson</p>
-                <p class="mt-1 text-base text-gray-600">Project Manager at Microsoft</p>
+                <p class="mt-6 text-base font-semibold text-black">Mariela AI</p>
+                <p class="mt-1 text-base text-gray-600">Werb sites and AI</p>
             </div>
 
             <div>
                 <div class="overflow-hidden bg-white">
                     <div class="p-8 lg:px-12 lg:py-10">
-                        <h3 class="text-2xl font-semibold text-black">Join 5,482 other developers</h3>
-                        <p class="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
+                        <h3 class="text-2xl font-semibold text-black">Welcome to Mariela AI</h3>
+                        <p class="mt-4 text-base text-gray-600">At Mariela AI, we specialize in designing and developing intelligent websites that elevate your digital presence.
+                            We merge elegant user interfaces with the power of artificial intelligence to create personalized, automated, and scalable solutions.
+                          </p></p>
 
                         <a href="#" title="" class="flex items-center justify-center w-full px-4 py-4 mt-8 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border-2 border-transparent rounded-md hover:bg-blue-700 focus:bg-blue-700" role="button">
                             Create your first website
                         </a>
 
                         <a href="#" title="" class="flex items-center justify-center w-full px-4 py-4 mt-4 text-base font-semibold text-black transition-all duration-200 bg-transparent border-2 border-black rounded-md hover:bg-black focus:bg-black hover:text-white focus:text-white" role="button">
-                            Talk to sales
+                            Contact Us
                         </a>
                     </div>
                 </div>
