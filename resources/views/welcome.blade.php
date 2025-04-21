eda35250c894441a6e8565bdee9f176607a49588
aimariela <contact@marielaai.com>
2025-04-20 21:18:47 -0500

    agrego presentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.navigation-welcome')	
 @section('content')
 
-
     <flux:main container>
         <flux:heading size="xl" level="1">Welcome to Mariela AI</flux:heading>
 
         <flux:text class="mt-2 mb-6 text-base">We are a group ofprofesional with agreat experience in create websites with AI applications</flux:text>
 
         <flux:separator variant="subtle" />
+        <x-presentation />
+        <flux:separator variant="subtle" class="mt-6" />
     </flux:main>
 
     @fluxScripts
