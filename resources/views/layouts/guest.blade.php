2b7fecaa3946a5575bed40fe518bb6cc090cf6ca
aimariela <contact@marielaai.com>
2025-04-21 13:55:23 -0500

    Agrego favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
 
         <!-- Scripts -->
     </head>
--- resources/views/layouts/navigation-welcome.blade.php
+++ resources/views/layouts/navigation-welcome.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 		<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
     @fluxAppearance
 
   </head>
