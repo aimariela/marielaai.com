55c4c4def39c4f1684c54aa69e9795b142e342e4
aimariela <contact@marielaai.com>
2025-04-20 20:52:56 -0500

    ensayo reverso

--- resources/views/layouts/navigation-welcome.blade.php
+++ resources/views/layouts/navigation-welcome.blade.php
@@ -14,6 +14,56 @@
 
   </head>
 	<body>
+    <flux:header container class="bg-zinc-50 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-700">
+
+    <flux:navbar class="-mb-px">
+
+        @if (Route::has('login'))
+                @auth
+        <flux:navbar.item icon="home" href="{{ url('/dashboard') }}" current>Dashboard</flux:navbar.item>
+                @else
+                    <a
+                        href="{{ route('login') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                    >
+                        Log in
+                    </a>
+
+                    @if (Route::has('register'))
+                        <a
+                            href="{{ route('register') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                            Register
+                        </a>
+                    @endif
+                @endauth
+            </nav>
+        @endif
+
+        <flux:separator vertical variant="subtle" class="my-2"/>
+
+        <flux:dropdown class="max-lg:hidden">
+            <flux:navbar.item icon:trailing="chevron-down">Favorites</flux:navbar.item>
+
+            <flux:navmenu>
+                <flux:navmenu.item href="#">Marketing site</flux:navmenu.item>
+                <flux:navmenu.item href="#">Android app</flux:navmenu.item>
+                <flux:navmenu.item href="#">Brand guidelines</flux:navmenu.item>
+            </flux:navmenu>
+        </flux:dropdown>
+    </flux:navbar>
+
+    <flux:spacer />
+
+    <flux:navbar class="me-4">
+        <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
+        <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
+        <flux:navbar.item class="max-lg:hidden" icon="information-circle" href="#" label="Help" />
+    </flux:navbar>
+
+</flux:header>
+<div class="container">
 @yield('content')
+</div>
 	</body>
 </html>
