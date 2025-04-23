83c9d194a1986873a363222e7ceb1bd6d560f824
aimariela <contact@marielaai.com>
2025-04-22 23:28:22 -0500

    crw

--- resources/views/components/flux/dark-mode-toggle.blade.php
+++ resources/views/components/flux/dark-mode-toggle.blade.php
@@ -1,5 +1,1 @@
-<flux:radio.group x-data x-model="$flux.appearance">
-    <flux:radio value="light">Light</flux:radio>
-    <flux:radio value="dark">Dark</flux:radio>
-    <flux:radio value="system">System</flux:radio>
-</flux:radio.group>
+<flux:switch x-data x-model="$flux.dark" label="Dark mode"  />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,8 @@
         <flux:text class="mt-2 mb-6 text-base">We are a group of profesional with agreat experience in create websites with AI applications</flux:text>
 
         <flux:separator variant="subtle" />
-        <x-presentation />
+        
         <flux:separator variant="subtle" class="mt-6" />
     </flux:main>
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif
+    <x-presentation />
 @endsection
