d13d3f169a00f6feea0d7eff4df0fc1a7ae5115f
aimariela <contact@marielaai.com>
2025-04-22 23:15:34 -0500

    cr2

--- resources/views/layouts/navigation-welcome.blade.php
+++ resources/views/layouts/navigation-welcome.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
         <link rel="icon" href="public/favicon-16x16.png" type="image/x-icon">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
     @fluxAppearance
 
